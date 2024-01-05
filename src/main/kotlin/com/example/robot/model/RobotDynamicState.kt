a933201c9f1ce8b1941e78c08ae5f85f5115dff8
akgaber@ejada.com <akgaber@ejada.com>
2024-01-05 23:32:22 +0200

    Add @DynamicUpdate to RobotDynamicState for efficient updates.

--- src/main/kotlin/com/example/robot/model/RobotDynamicState.kt
+++ src/main/kotlin/com/example/robot/model/RobotDynamicState.kt
@@ -3,11 +3,13 @@ package com.example.robot.model
 import com.example.robot.model.enums.RobotState
 import com.example.robot.model.enums.converters.RobotStateConverter
 import jakarta.persistence.*
+import org.hibernate.annotations.DynamicUpdate
 import org.hibernate.annotations.OptimisticLockType
 import org.hibernate.annotations.OptimisticLocking
 
 @Entity
 @Table(name = "ROBOT_DYNAMIC_STATE")
+@DynamicUpdate(true)
 @OptimisticLocking(type = OptimisticLockType.DIRTY)
 data class RobotDynamicState (
     @Id
@@ -20,5 +22,4 @@ data class RobotDynamicState (
     @Column(name = "ROBOT_STATE")
     @Convert(converter = RobotStateConverter::class)
     var robotState : RobotState
-){
-}
+){}
