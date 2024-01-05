641783b9f5ee4be29e05cc7d69e131a2fc4f7156
akgaber@ejada.com <akgaber@ejada.com>
2024-01-05 23:44:08 +0200

    Add logic to handle loading a robot with medications and retrieving loaded medications for a given robot.

--- src/main/kotlin/com/example/robot/service/impl/CarriedMedicationHashmap.kt
+++ src/main/kotlin/com/example/robot/service/impl/CarriedMedicationHashmap.kt
@@ -1,19 +1,64 @@
 package com.example.robot.service.impl
 
+import com.example.robot.exception.RobotCannotBeLoadedException
+import com.example.robot.exception.RobotNotFoundException
+import com.example.robot.exception.WeightLimitExceededException
+import com.example.robot.model.Robot
+import com.example.robot.model.RobotDynamicState
+import com.example.robot.model.enums.RobotState
 import com.example.robot.repository.CarriedMedicationRepository
+import com.example.robot.repository.MedicationRepository
+import com.example.robot.repository.RobotDynamicStateRepository
+import com.example.robot.repository.RobotRepository
 import com.example.robot.service.CarriedMedication
+import jakarta.transaction.Transactional
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
-class CarriedMedicationHashmap(private val carriedMedicationRepository: CarriedMedicationRepository)
-        : CarriedMedication {
+class CarriedMedicationHashmap(
+        private val robotRepository: RobotRepository,
+        private val robotDynamicStateRepository: RobotDynamicStateRepository,
+        private val medicationRepository: MedicationRepository,
+        private val carriedMedicationRepository: CarriedMedicationRepository
+    ) : CarriedMedication
+{
+    @Transactional
     override fun loadRobotWithMedication(serialNumber: String, medicationNames: List<String>): Unit {
-        carriedMedicationRepository.storeLoadedMedication(serialNumber, medicationNames)
-        //TODO mark caller with @Transactional because it will change the state of the robot to Loading in DB
-        //which should be altered if operation failed.
+        val robot : Robot = robotRepository.findByIdOrNull(serialNumber) ?: throw RobotNotFoundException()
+        canLoadMedications(robot, medicationNames)
+        loadRobot(robot, medicationNames)
+    }
+
+    private fun canLoadMedications(
+        robot: Robot,
+        medicationNames: List<String>
+    )
+    {
+        val robotDynamicState : RobotDynamicState = robot.robotDynamicState
+        if(robotDynamicState.batteryCapacity <= 25 ||
+            RobotState.IDLE != robotDynamicState.robotState)
+                throw RobotCannotBeLoadedException()
+
+        val medicationsWeight : Int = medicationRepository.findMedicationsWeight(medicationNames)
+        if(medicationsWeight > robot.weightLimit)
+            throw  WeightLimitExceededException()
+    }
+
+    private fun loadRobot(robot: Robot, medicationNames: List<String>){
+        changeRobotState(robot, RobotState.LOADING)
+        carriedMedicationRepository.storeLoadedMedication(robot.serialNumber, medicationNames)
+        changeRobotState(robot, RobotState.LOADED)
+    }
+
+    private fun changeRobotState(robot : Robot, newState : RobotState){
+        robot.robotDynamicState.robotState = newState
+        robotDynamicStateRepository.save(robot.robotDynamicState)
     }
 
     override fun getLoadedMedication(serialNumber: String): List<String>? {
         return carriedMedicationRepository.getLoadedMedication(serialNumber)
+        //TODO handle the null response
     }
+
 }
