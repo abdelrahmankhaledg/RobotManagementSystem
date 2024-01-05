f414a31022a73f9586bc4c6a9e9bf9e8e41fc8dc
akgaber@ejada.com <akgaber@ejada.com>
2024-01-05 23:34:27 +0200

    Add query to find the total weight of supplied medicationNames

--- src/main/kotlin/com/example/robot/repository/MedicationRepository.kt
+++ src/main/kotlin/com/example/robot/repository/MedicationRepository.kt
@@ -1,9 +1,12 @@
 package com.example.robot.repository
 
 import com.example.robot.model.Medication
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface MedicationRepository : CrudRepository<Medication, String>{
+    @Query("SELECT SUM(M.WEIGHT) FROM MEDICATION M WHERE MEDICATION_NAME IN :medicationNames")
+    fun findMedicationsWeight(medicationNames : List<String>) : Int
 }
