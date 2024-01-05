25984df0627a2326e18b4121469f230d5a5c50f0
akgaber@ejada.com <akgaber@ejada.com>
2024-01-05 19:54:03 +0200

    Add @OptimisticLocking for RobotDynamicState.
    To handle the case where two clients want to load the same robot with medications. In such case, one of the clients would be able to set the state of the robot as "LOADING" while the other would fail due to OptimisticLockingException.

--- src/main/kotlin/com/example/robot/model/RobotDynamicState.kt
+++ src/main/kotlin/com/example/robot/model/RobotDynamicState.kt
@@ -3,10 +3,17 @@ package com.example.robot.model
 import com.example.robot.model.enums.RobotState
 import com.example.robot.model.enums.converters.RobotStateConverter
 import jakarta.persistence.*
+import org.hibernate.annotations.OptimisticLockType
+import org.hibernate.annotations.OptimisticLocking
 
 @Entity
 @Table(name = "ROBOT_DYNAMIC_STATE")
+@OptimisticLocking(type = OptimisticLockType.DIRTY)
 data class RobotDynamicState (
+    @Id
+    @Column(name = "SERIAL_NUMBER")
+    val serialNumber : String,
+
     @Column(name = "BATTERY_CAPACITY")
     var batteryCapacity : Int,
 
