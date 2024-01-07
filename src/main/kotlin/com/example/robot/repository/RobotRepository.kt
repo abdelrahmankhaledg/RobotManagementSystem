efd5ed4cd797a59f05beb6a03885da6d463dce03
akgaber@ejada.com <akgaber@ejada.com>
2024-01-07 20:29:46 +0200

    Add queries to RobotRepository

--- src/main/kotlin/com/example/robot/repository/RobotRepository.kt
+++ src/main/kotlin/com/example/robot/repository/RobotRepository.kt
@@ -1,9 +1,16 @@
 package com.example.robot.repository
 
 import com.example.robot.model.Robot
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface RobotRepository : CrudRepository<Robot, String> {
+    @Query("SELECT R.batteryCapacity from ROBOT R WHERE R.serialNumber = :serialNumber")
+    fun findBatteryCapacity(serialNumber : String): Int
+
+
+    @Query("SELECT R from ROBOT R where R.robotState = 'IDLE' and R.batteryCapacity > 25")
+    fun findAvailableRobotsForLoading() : List<Robot>
 }
