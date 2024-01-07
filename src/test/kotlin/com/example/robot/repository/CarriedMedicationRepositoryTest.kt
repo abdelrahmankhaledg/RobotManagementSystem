0e0aba2278fe8d3279cc8fd49a4e7908f8e362ae
akgaber@ejada.com <akgaber@ejada.com>
2024-01-07 09:23:54 +0200

    Add unit test for CarriedMedicationRepository

---
+++ src/test/kotlin/com/example/robot/repository/CarriedMedicationRepositoryTest.kt
@@ -0,0 +1,27 @@
+package com.example.robot.repository
+
+import com.example.robot.repository.impl.CarriedMedicationHashmapRepository
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class CarriedMedicationRepositoryTest() {
+    private val carriedMedicationRepository: CarriedMedicationHashmapRepository = CarriedMedicationHashmapRepository()
+    private val serialNumber : String = "ABC123"
+    private val medicationNames : List<String> = arrayListOf("PANADOL", "ADOL", "BRUFEN")
+
+    @BeforeEach
+    fun populateData(){
+        carriedMedicationRepository.storeLoadedMedication(serialNumber, medicationNames)
+    }
+
+    @Test
+    fun getLoadedMedicationHappyTest(){
+        assert(carriedMedicationRepository.getLoadedMedication(serialNumber) == medicationNames)
+    }
+
+    @Test
+    fun getLoadedMedicationOfNotLoadedRobot(){
+        val notLoadedRobotSerialNumber = "abc"
+        assert(carriedMedicationRepository.getLoadedMedication(notLoadedRobotSerialNumber) == null)
+    }
+}
