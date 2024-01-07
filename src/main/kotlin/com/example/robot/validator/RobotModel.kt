5f03fc1686540025191eeb106872a17c0d488a43
akgaber@ejada.com <akgaber@ejada.com>
2024-01-07 21:49:47 +0200

    Fix validation logic and error message

--- src/main/kotlin/com/example/robot/validator/RobotModel.kt
+++ src/main/kotlin/com/example/robot/validator/RobotModel.kt
@@ -2,15 +2,16 @@ package com.example.robot.validator
 
 
 import jakarta.validation.Constraint
-import org.intellij.lang.annotations.RegExp
+import jakarta.validation.constraints.Pattern
 import kotlin.reflect.KClass
-@RegExp(prefix = "^(LIGHTWEIGHT|MIDDLEWEIGHT|CRUISERWEIGHT|HEAVYWEIGHT)\$")
+@Pattern(regexp = "^(LIGHTWEIGHT|MIDDLEWEIGHT|CRUISERWEIGHT|HEAVYWEIGHT)\$",
+    message = "The robot model can only be one of LIGHTWEIGHT, MIDDLEWEIGHT," +
+        "CRUISERWEIGHT OR HEAVYWEIGHT")
 @Target(AnnotationTarget.FIELD, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 @Retention(AnnotationRetention.RUNTIME)
 @Constraint(validatedBy = [])
 annotation class RobotModel(
-    val message: String = "The robot model can only be one of LIGHTWEIGHT, MIDDLEWEIGHT," +
-            "CRUISERWEIGHT OR HEAVYWEIGHT",
+    val message: String = "",
     val groups: Array<KClass<out Any>> = [],
     val payload: Array<KClass<out Any>> = []
 )
--- src/main/kotlin/com/example/robot/validator/RobotState.kt
+++ src/main/kotlin/com/example/robot/validator/RobotState.kt
@@ -2,15 +2,16 @@ package com.example.robot.validator
 
 
 import jakarta.validation.Constraint
+import jakarta.validation.constraints.Pattern
 import org.intellij.lang.annotations.RegExp
 import kotlin.reflect.KClass
-@RegExp(prefix = "^(IDLE|LOADING|LOADED|DELIVERING|DELIVERED|RETURNING)\$")
+@Pattern(regexp = "^(IDLE|LOADING|LOADED|DELIVERING|DELIVERED|RETURNING)\$0", message = "The robot state can only be one of IDLE, LOADING, LOADED" +
+        "DELIVERING, DELIVERED OR RETURNING")
 @Target(AnnotationTarget.FIELD, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 @Retention(AnnotationRetention.RUNTIME)
 @Constraint(validatedBy = [])
 annotation class RobotState(
-    val message: String = "The robot state can only be one of IDLE, LOADING, LOADED" +
-            "DELIVERING, DELIVERED OR RETURNING",
+    val message: String = "",
     val groups: Array<KClass<out Any>> = [],
     val payload: Array<KClass<out Any>> = []
 )
