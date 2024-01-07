b3de0da9ba716d421c9177dd20feb58c0d1023e5
akgaber@ejada.com <akgaber@ejada.com>
2024-01-08 00:05:53 +0200

    Add query to retrieve robot serial number along with the battery capacity of the robot

--- src/main/kotlin/com/example/robot/repository/RobotDynamicStateRepository.kt
+++ src/main/kotlin/com/example/robot/repository/RobotDynamicStateRepository.kt
@@ -1,9 +1,13 @@
 package com.example.robot.repository
 
+import com.example.robot.logging.model.BatteryLogEntry
 import com.example.robot.model.RobotDynamicState
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface RobotDynamicStateRepository : CrudRepository<RobotDynamicState, String>{
+    @Query("SELECT new com.example.robot.logging.model.BatteryLogEntry(RDS.serialNumber, RDS.batteryCapacity) FROM RobotDynamicState RDS")
+    fun findBatteryLevelForAllRobots() : List<BatteryLogEntry>
 }
