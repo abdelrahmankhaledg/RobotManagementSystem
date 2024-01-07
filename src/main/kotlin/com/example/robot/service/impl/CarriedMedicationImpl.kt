44e5046079d5b96742e237bc551d1d42e04854f7
akgaber@ejada.com <akgaber@ejada.com>
2024-01-07 08:36:22 +0200

    Rename CarriedMedication service

--- src/main/kotlin/com/example/robot/service/impl/CarriedMedicationImpl.kt
+++ src/main/kotlin/com/example/robot/service/impl/CarriedMedicationImpl.kt
@@ -16,7 +16,7 @@ import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
-class CarriedMedicationHashmap(
+class CarriedMedicationImpl(
         private val robotRepository: RobotRepository,
         private val robotDynamicStateRepository: RobotDynamicStateRepository,
         private val medicationRepository: MedicationRepository,
