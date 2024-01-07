af7d9042cf2b17d24a50bb2b10207e620e076bb0
akgaber@ejada.com <akgaber@ejada.com>
2024-01-07 21:50:11 +0200

    Fix typo

--- src/main/kotlin/com/example/robot/validator/RobotState.kt
+++ src/main/kotlin/com/example/robot/validator/RobotState.kt
@@ -5,7 +5,7 @@ import jakarta.validation.Constraint
 import jakarta.validation.constraints.Pattern
 import org.intellij.lang.annotations.RegExp
 import kotlin.reflect.KClass
-@Pattern(regexp = "^(IDLE|LOADING|LOADED|DELIVERING|DELIVERED|RETURNING)\$0", message = "The robot state can only be one of IDLE, LOADING, LOADED" +
+@Pattern(regexp = "^(IDLE|LOADING|LOADED|DELIVERING|DELIVERED|RETURNING)\$", message = "The robot state can only be one of IDLE, LOADING, LOADED" +
         "DELIVERING, DELIVERED OR RETURNING")
 @Target(AnnotationTarget.FIELD, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 @Retention(AnnotationRetention.RUNTIME)
