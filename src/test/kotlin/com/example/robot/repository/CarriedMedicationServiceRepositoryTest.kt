6001a132017961616b3b33d4f06807146cbaab6a
akgaber@ejada.com <akgaber@ejada.com>
2024-01-07 21:06:16 +0200

    Fix testcase

--- src/test/kotlin/com/example/robot/repository/CarriedMedicationServiceRepositoryTest.kt
+++ src/test/kotlin/com/example/robot/repository/CarriedMedicationServiceRepositoryTest.kt
@@ -22,6 +22,6 @@ class CarriedMedicationServiceRepositoryTest() {
     @Test
     fun getLoadedMedicationOfNotLoadedRobot(){
         val notLoadedRobotSerialNumber = "abc"
-        assert(carriedMedicationRepository.getLoadedMedication(notLoadedRobotSerialNumber) == null)
+        assert(carriedMedicationRepository.getLoadedMedication(notLoadedRobotSerialNumber) == emptyList<String>())
     }
 }
