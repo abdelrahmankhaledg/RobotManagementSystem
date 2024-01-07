3a017e4423676102c674452a66fce8d282f9b28f
akgaber@ejada.com <akgaber@ejada.com>
2024-01-07 14:25:34 +0200

    Add @Repository for CarriedMedicationHashmapRepository

--- src/main/kotlin/com/example/robot/repository/impl/CarriedMedicationHashmapRepository.kt
+++ src/main/kotlin/com/example/robot/repository/impl/CarriedMedicationHashmapRepository.kt
@@ -1,8 +1,10 @@
 package com.example.robot.repository.impl
 
 import com.example.robot.repository.CarriedMedicationRepository
+import org.springframework.stereotype.Repository
 import java.util.concurrent.ConcurrentHashMap
 
+@Repository
 class CarriedMedicationHashmapRepository(
     private val carriedMedication : ConcurrentHashMap<String, List<String>> = ConcurrentHashMap()
 
